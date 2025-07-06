c8ad440d2fbab4c7babab28b5b8c3f2b71bd4e82
Fabian Holler <fabianholler2005@gmail.com>
2025-07-06 17:24:36 +0000

    Added logic for constructing the intitial sync message

--- service/sync.go
+++ service/sync.go
@@ -4,8 +4,8 @@ import (
 	"JustSync/snapshot"
 	"JustSync/utils"
 	"JustSync/websocket"
-	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
 )
 
@@ -13,10 +13,17 @@ func PrepareInitialSync(c *websocket.Client) ([]snapshot.SyncFileMessage, error)
 	path := utils.GetHostConfig().Application.Path
 	var messages []snapshot.SyncFileMessage
 
+	// Append start sync msg
+	startSyncMsg := snapshot.SyncFileMessage{
+		Payload: &snapshot.SyncFileMessage_StartSync{},
+	}
+	messages = append(messages, startSyncMsg)
+
+	// Append sync msg's for each file
 	if err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			utils.LogError("Error traversing full project for initial sync: %s", err.Error())
-			return fmt.Errorf("access error at %s: %w", path, err)
+			return err
 		}
 
 		// Skip directories
@@ -24,13 +31,33 @@ func PrepareInitialSync(c *websocket.Client) ([]snapshot.SyncFileMessage, error)
 			return nil
 		}
 
-		// TODO: Prepare and append file msg
+		fileContent, err := os.ReadFile(path)
+		if err != nil {
+			utils.LogError("Error reading file at %s: %s", path, err.Error())
+			return err
+		}
+
+		fileSync := &snapshot.FileSync{
+			Checksum: utils.CreateBlake3Hash(fileContent),
+			Path:     path,
+			Content:  fileContent,
+		}
+		syncMsg := snapshot.SyncFileMessage{
+			Payload: &snapshot.SyncFileMessage_File{File: fileSync},
+		}
+		messages = append(messages, syncMsg)
 
 		return nil
 	}); err != nil {
 		utils.LogError("Error traversing full project for initial sync: %s", err.Error())
 		return messages, err
 	}
+
+	// Append end sync msg
+	endSyncMsg := snapshot.SyncFileMessage{
+		Payload: &snapshot.SyncFileMessage_EndSync{},
+	}
+	messages = append(messages, endSyncMsg)
 
 	return messages, nil
 }
